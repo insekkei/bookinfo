4b7d2b2f480e7d9535049107e7d65c40dd4528da
Wang Long <wanglong@laoqinren.net>
2016-01-22 23:01:32 +0800

    run go fmt to format the code style
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

--- types.go
+++ types.go
@@ -7,59 +7,59 @@
 
 package main
 
-type	Rate struct {
-	Max			int			`json:"max"`
-	NumRater	int			`json:"numRater"`
-	Average		string		`json:"average"`
-	Min			int			`json:"min"`
+type Rate struct {
+	Max      int    `json:"max"`
+	NumRater int    `json:"numRater"`
+	Average  string `json:"average"`
+	Min      int    `json:"min"`
 }
 
-type	Tag struct {
-	Count		int		`json:"count"`
-	Name		string	`json:"name"`
-	Title		string	`json:"title"`
+type Tag struct {
+	Count int    `json:"count"`
+	Name  string `json:"name"`
+	Title string `json:"title"`
 }
 
-type	Book struct {
-	Rating			Rate		`json:"rating"`
-	Subtitle		string		`json:"subtitle"`
-	Author			[]string	`json:"author"`
-	Pubdate			string		`json:"pubdate"`
-	Tags			[]Tag		`json:"tags"`
-	Origin_title	string		`json:"origin_title"`
-	Image			string		`json:"image"`
-	Binding			string		`json:"binding"`
-	Translator		[]string	`json:"translator"`
-	Catalog			string		`json:"catalog"`
-	Pages			string		`json:"pages"`
-	Images			map[string]string			`json:"images"`
-	Alt				string			`json:"alt"`
-	Id				string			`json:"id"`
-	Publisher		string			`json:"publisher"`
-	Isbn10			string			`json:"isbn10"`
-	Isbn13			string			`json:"isbn13"`
-	Title			string			`json:"title"`
-	Url				string			`json:"url"`
-	Alt_title		string			`json:"alt_title"`
-	Author_intro	string			`json:"author_intro"`
-	Summary			string			`json:"summary"`
-	Series			map[string]string		`json:"series"`
-	Price			string			`json:"price"`
+type Book struct {
+	Rating       Rate              `json:"rating"`
+	Subtitle     string            `json:"subtitle"`
+	Author       []string          `json:"author"`
+	Pubdate      string            `json:"pubdate"`
+	Tags         []Tag             `json:"tags"`
+	Origin_title string            `json:"origin_title"`
+	Image        string            `json:"image"`
+	Binding      string            `json:"binding"`
+	Translator   []string          `json:"translator"`
+	Catalog      string            `json:"catalog"`
+	Pages        string            `json:"pages"`
+	Images       map[string]string `json:"images"`
+	Alt          string            `json:"alt"`
+	Id           string            `json:"id"`
+	Publisher    string            `json:"publisher"`
+	Isbn10       string            `json:"isbn10"`
+	Isbn13       string            `json:"isbn13"`
+	Title        string            `json:"title"`
+	Url          string            `json:"url"`
+	Alt_title    string            `json:"alt_title"`
+	Author_intro string            `json:"author_intro"`
+	Summary      string            `json:"summary"`
+	Series       map[string]string `json:"series"`
+	Price        string            `json:"price"`
 }
 
-type	BookEntry struct {
-	Id			string			`json:"id"`
-	Title		string			`json:"title"`
-	Author		[]string		`json:"author"`
-	Image		string			`json:"image"`
-	Rating		string			`json:"rating"`
-	Publisher	string			`json:"publisher"`
-	Pages		string			`json:"pages"`
-	Price		string			`json:"price"`
+type BookEntry struct {
+	Id        string   `json:"id"`
+	Title     string   `json:"title"`
+	Author    []string `json:"author"`
+	Image     string   `json:"image"`
+	Rating    string   `json:"rating"`
+	Publisher string   `json:"publisher"`
+	Pages     string   `json:"pages"`
+	Price     string   `json:"price"`
 }
 
-type	Summary struct {
-	Name	string			`json:"library"`
-	Total	int				`json:"total"`
-	Books	[]BookEntry		`json:"books"`
+type Summary struct {
+	Name  string      `json:"library"`
+	Total int         `json:"total"`
+	Books []BookEntry `json:"books"`
 }
